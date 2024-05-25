ad380039ec69ff40bb4b5ec1b2afc27492466df1
andrewandreii <andrei3f@yahoo.com>
2024-05-25 19:22:47 +0300

    Just for testing

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 
 #include "tokenizer.h"
+#include "parser.h"
 
 int main() {
-    FILE *f = fopen("bf_test.bfc", "r");
+    FILE *f = fopen("bf_find_example.bf", "r");
+
     token_t *tokens = tokenize(f);
 
     int i;
-    for (i = 0; tokens[i].type != -1; ++ i) {
+    for (i = 0; tokens[i].type != EOF_TOKEN; ++ i) {
         log_token(stdout, tokens + i);
     }
+
+    init_parser();
+
+    parse_program(tokens);
 }
